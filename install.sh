be42901fbdf2acc75dfc7628ddd9105a3f342d83
kvaps <kvapss@gmail.com>
2018-07-17 18:10:55 +0200

    first commit

---
+++ install.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+set -o errexit
+set -o pipefail
+
+VENDOR=kvaps
+DRIVER=block
+
+# Install driver
+driver_dir=$VENDOR${VENDOR:+"~"}${DRIVER}
+mkdir -p "/flexmnt/$driver_dir"
+cp "/jq" "/flexmnt/$driver_dir/.jq"
+mv -f "/flexmnt/$driver_dir/.jq" "/flexmnt/$driver_dir/jq"
+cp "/$DRIVER" "/flexmnt/$driver_dir/.$DRIVER"
+mv -f "/flexmnt/$driver_dir/.$DRIVER" "/flexmnt/$driver_dir/$DRIVER"
+
+# Update old driver also
+for LN_DRIVER in nbd loop sheepdog; do
+    ln_driver_dir=$VENDOR${VENDOR:+"~"}${LN_DRIVER}
+    mkdir -p "/flexmnt/$ln_driver_dir"
+    ln -sf "../$driver_dir/$DRIVER" "/flexmnt/$ln_driver_dir/$LN_DRIVER"
+done
+
+# Sleep calm
+while : ; do
+  sleep 3600
+done
